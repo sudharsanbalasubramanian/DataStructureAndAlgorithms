2d2f90457444da4ba92325648acb35a03e289ab4
sudharsanbalasubramanian <sudharsanbalasubramanian1@gmail.com>
2026-02-18 18:27:56 +0530

    Add TwoSum and Sudoku validation utilities
    Added PairSumUnSorted.cs with a TwoSum method for finding a pair of numbers in an unsorted array that sum to a target using a hash map. Added VerifySudokuBoard.cs with an IsValidSudoku method to validate 9x9 Sudoku boards using hash sets for rows, columns, and boxes.

---
+++ HashMapAndSet/PairSumUnSorted.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataStructureAndAlgorithms.HashMapAndSet;
+
+public static class PairSumUnSorted
+{
+    public static int[] TwoSum(int[] arr, int target)
+    {
+        Dictionary<int, int> map = [];
+
+        for (int i = 0; i < arr.Length; i++)
+        {
+            int needed = target - arr[i];
+
+            if (map.ContainsKey(needed))
+            {
+                return [needed, arr[i]];
+            }
+
+            map[arr[i]] = i;
+        }
+
+        return [];
+    }
+}
---
+++ HashMapAndSet/VerifySudokuBoard.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataStructureAndAlgorithms.HashMapAndSet;
+
+public static class VerifySudokuBoard
+{
+    public static bool IsValidSudoku(int[][] board)
+    {
+        HashSet<int>[] rows = new HashSet<int>[9];
+        HashSet<int>[] cols = new HashSet<int>[9];
+        HashSet<int>[] boxes = new HashSet<int>[9];
+
+        for (int i = 0; i < 9; i++)
+        {
+            rows[i] = new HashSet<int>();
+            cols[i] = new HashSet<int>();
+            boxes[i] = new HashSet<int>();
+        }
+
+        for (int r = 0; r < 9; r++)
+        {
+            for (int c = 0; c < 9; c++)
+            {
+                int val = board[r][c];
+
+                if (val == 0)
+                    continue;
+
+                // Optional safety check
+                if (val < 1 || val > 9)
+                    return false;
+
+                int boxIndex = (r / 3) * 3 + (c / 3);
+
+                if (rows[r].Contains(val) ||
+                    cols[c].Contains(val) ||
+                    boxes[boxIndex].Contains(val))
+                {
+                    return false;
+                }
+
+                rows[r].Add(val);
+                cols[c].Add(val);
+                boxes[boxIndex].Add(val);
+            }
+        }
+
+        return true;
+    }
+}
